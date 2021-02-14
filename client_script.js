fba94bad55d1e4ed4432ec46107f46380472e883
Kostiantyn Kolchenko <kolchkos@fit.cvut.cz>
2021-02-14 12:08:12 +0100

    List sorting

--- client_script.js
+++ client_script.js
@@ -4,10 +4,6 @@ let search_form = document.getElementById("search_form_id");
 // let list = document.getSelection(".list");
 let list = document.getElementsByClassName("list")[0];
 
-function create_form_close() {
-    document.getElementById('create_modal_id').style.display='none';
-}
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     const modal = document.getElementById("create_modal_id");
@@ -56,6 +52,10 @@ search_form.addEventListener("submit", function(e){
     get_request();
 });
 
+function create_form_close() {
+    document.getElementById('create_modal_id').style.display='none';
+}
+
 async function get_request() {
     url = "http://localhost:8080/api/v1/books?page=0&size=6";
     
@@ -67,7 +67,6 @@ async function get_request() {
         }
         }).then( async (response) => {
             const json = await response.json();
-            // console.log('Response: ', json);
 
             // removing all li tags from ul
             $(list).empty(); 
@@ -77,6 +76,7 @@ async function get_request() {
                 for( i = 0; i < json._embedded.bookDtoList.length; i++ ){
                     create_block(json._embedded.bookDtoList[i]);
                 }
+                list_sort( list );
             }
         }).catch( (error) => {
             console.error('Error: ', error);
@@ -89,20 +89,40 @@ function create_block( book_json ){
     bookId = bookName.replace(/\s/g, '') + issueNumber;
     satisfactionScore = book_json.satisfactionScore;
 
-    // console.log(json);
     let li = document.createElement("li");
     li.classList.add("book");
     let HTMLbox = `
         <h2 class="name">
             <span>${bookName}#${issueNumber}</span>
         </h2>
-        <div vclass="satisfactionScore"><sup>${satisfactionScore}</sup></div>
+        <div class="satisfactionScore"><sup>${satisfactionScore}</sup></div>
         <button class="upratebtn" id="${bookId}" onclick="upRate(this)">Up</button>
         <button class="downratebtn" id="${bookId}" onclick="downRate(this)">Down</button>
     `;
     li.innerHTML = HTMLbox;
     // li.style.backgroundImage = cover;    <img>
     list.appendChild(li);
+}
+
+function list_sort( ul ){
+    var new_ul = ul.cloneNode(false);
+
+    var li_array = [];
+    for(var i = ul.childNodes.length; i--;){
+        if(ul.childNodes[i].nodeName === 'LI')
+        li_array.push(ul.childNodes[i]);
+    }
+
+    // console.log(li_array[0].getElementsByClassName("satisfactionScore")[0].childNodes[0].innerText);
+    // Sort the li_array in descending order
+    li_array.sort(function(a, b){
+        return parseInt(b.getElementsByClassName("satisfactionScore")[0].childNodes[0].innerText , 10) - 
+                parseInt(a.getElementsByClassName("satisfactionScore")[0].childNodes[0].innerText , 10);
+    });
+
+    for(var i = 0; i < li_array.length; i++)
+        new_ul.appendChild(li_array[i]);
+    ul.parentNode.replaceChild(new_ul, ul);
 }
 
 function upRate( book ){
